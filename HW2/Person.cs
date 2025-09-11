e394745375576650207ef3ad3643525f3475877f
Sang852 <sangd6216l@gmail.com>
2025-09-11 20:15:29 +0700

    Add HW2 project to github

---
+++ HW2/Person.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2
+{
+    internal class Person
+    {
+        private string name;
+        private string address;
+        private double salary;
+
+        public Person()
+        {
+            name = "";
+            address = "";
+            salary = 0;
+        }
+        public Person(string name, string address, double salary)
+        {
+            this.name = name;
+            this.address = address;
+            this.salary = salary;
+        }
+
+        public string Name
+        {
+            get { 
+                return name; 
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string Address
+        {
+            get
+            {
+                return address;
+            }
+            set
+            {
+                address = value;
+            }
+        }
+        public double Salary
+        {
+            get
+            {
+                return salary;
+            }
+            set
+            {
+                salary = value;
+            }
+        }
+        public Person inputPersonInfo()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Please input name: ");
+                    name = Console.ReadLine();
+                    if (string.IsNullOrEmpty(name))
+                        throw new Exception("You must input Name.");
+                    break;
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Please input address: ");
+                    address = Console.ReadLine();
+                    if (string.IsNullOrEmpty(address))
+                        throw new Exception("You must input Name.");
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            
+
+            string sSalary;
+            double salary;
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Please input salary: ");
+                    sSalary = Console.ReadLine();
+                    if (string.IsNullOrEmpty(sSalary))
+                        throw new Exception("You must input Salary.");
+                    if (!double.TryParse(sSalary, out salary))
+                        throw new Exception("You must input digit.");
+                    if (salary <= 0)
+                        throw new Exception("Salary is greater than zero.");
+                    break;
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+
+            return new Person(name, address, salary);
+        }
+
+        public void displayPersonInfo(Person person)
+        {
+            Console.WriteLine("Information of Person you have entered: ");
+            Console.WriteLine($"Name: {person.name}");
+            Console.WriteLine($"Address: {person.address}");
+            Console.WriteLine($"Salary: {person.salary}");
+            Console.WriteLine();
+        }
+
+        public Person[] sortBySalary(Person[] persons)
+        {
+            if (persons.Length == 0 || persons == null)
+            {
+                throw new Exception("Can't Sort Person");
+            }
+            for (int i = 0; i < persons.Length - 1; i++)
+            {
+                for (int j = 0; j < persons.Length - i - 1; j++)
+                {
+                    if (persons[j].salary > persons[j + 1].salary)
+                    {
+                        Person tmp = persons[j];
+                        persons[j] = persons[j + 1];
+                        persons[j + 1] = tmp;
+                    }
+                }
+            }
+            return persons;
+        }
+    }
+}
--- HW2/Program.cs
+++ HW2/Program.cs
@@ -1,10 +1,66 @@
-﻿internal class Program
+﻿using System;
+using System.Text;
+namespace HW2
 {
-    
-    private static void Main(string[] args)
+    internal class Program
     {
-        MyDate md1 = new MyDate(DateTime.Now.Day,DateTime.Now.Month, DateTime.Now.Year);
-        Console.WriteLine("{0}/{1}/{2}",md1.Day,md1.Month,md1.Year);
-        Console.ReadKey();
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+            
+            int n;
+            Person[] persons;
+            while (true)
+            {
+               try
+               {
+                    Console.Write("Enter the number of people: ");
+                    string num = Console.ReadLine();
+                    if (string.IsNullOrEmpty(num))
+                        throw new Exception("You cannot leave blank.");
+                    if (!int.TryParse(num, out n))
+                        throw new Exception("You must input digit.");
+                    if (n <= 0)
+                        throw new Exception("Number of people must be positive integer.");
+                    persons = new Person[n];
+                    break;
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            Console.Write("Press any key to continue...");
+            Console.ReadKey();
+            Console.Clear();
+            Person person = new Person();
+
+            Console.WriteLine("=====Management Person Program=====");
+
+            // Nhập 3 người
+            for (int i = 0; i < persons.Length; i++)
+            {
+                Console.WriteLine("Input Information of Person");
+                persons[i] = person.inputPersonInfo();
+            }
+
+            // Sắp xếp
+            try
+            {
+                persons = person.sortBySalary(persons);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            Console.WriteLine();
+            foreach (Person p in persons)
+            {
+                person.displayPersonInfo(p);
+            }
+        }
     }
 }
